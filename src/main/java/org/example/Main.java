4a2f47dc83e9d7b4f22c1b3468c0554cf59a3171
Marek Brandt <marek7119@gmail.com>
2022-12-27 00:24:58 +0100

    add gitignore

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Board board = new Board(new BoardSize(10, 10));
+        board.show();
+
+        boolean simulationShouldRun = true;
+        while(simulationShouldRun) {
+
+        }
+    }
+}
